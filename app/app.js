db97bc4e0e4d263d9191a29156d1cd375212432f
Timothy Heider <tim@preludevoice.com>
2015-09-30 10:21:59 -0700

    banner

--- app/app.js
+++ app/app.js
@@ -6,7 +6,7 @@ var THREE = require('three');
 // app.
 function initApp() {
   var renderer = new THREE.WebGLRenderer();
-  renderer.setSize(800, 600);
+  renderer.setSize(1024, 768);
   var canvasElement = document.getElementById('canvas-3d');
   canvasElement.appendChild(renderer.domElement);
 
@@ -14,23 +14,23 @@ function initApp() {
 
   var camera = new THREE.PerspectiveCamera(
     35,             // Field of view
-    800 / 600,      // Aspect ratio
+    1024 / 768,      // Aspect ratio
     0.1,            // Near plane
     10000           // Far plane
   );
   camera.position.set(-15, 10, 10);
   camera.lookAt(scene.position);
 
-  var geometry = new THREE.BoxGeometry(5, 5, 5);
-  var material = new THREE.MeshLambertMaterial({color: 0xFF0000});
+  var geometry = new THREE.BoxGeometry(3, 3, 3);
+  var material = new THREE.MeshLambertMaterial({color: 0xFF4422});
   var mesh = new THREE.Mesh(geometry, material);
   scene.add(mesh);
 
-  var light = new THREE.PointLight(0xFFFF00);
+  var light = new THREE.PointLight(0xFFFFCC);
   light.position.set(10, 0, 10);
   scene.add(light);
 
-  renderer.setClearColor(0xdddddd, 1);
+  renderer.setClearColor(0xeeeeee, 1);
   renderer.render(scene, camera);
 };
 
