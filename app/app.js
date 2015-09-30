a1523960507b0ee0a2b8eb07ebd65bddd7270741
Timothy Heider <tim@preludevoice.com>
2015-09-30 15:43:34 -0700

    terrain grid rendered as wireframe

--- app/app.js
+++ app/app.js
@@ -30,38 +30,45 @@ app.controller("TerrainAppController", function($scope) {
 
     console.log('generating new plane: widthPels=' + $scope.gridWidthPixels + ', heightPels=' + $scope.gridHeightPixels + ' gridPointCount=' + $scope.gridPointCount);
     var geometry = new THREE.Geometry();
-    // create 4 corner points
-    var p0 = new THREE.Vector3( -1 * $scope.gridWidthPixels,  0, -1 * $scope.gridHeightPixels );
-    var p1 = new THREE.Vector3( -1 * $scope.gridWidthPixels,  0, +1 * $scope.gridHeightPixels );
-    var p2 = new THREE.Vector3( +1 * $scope.gridWidthPixels,  0, +1 * $scope.gridHeightPixels );
-    var p3 = new THREE.Vector3( +1 * $scope.gridWidthPixels,  0, -1 * $scope.gridHeightPixels );
-    console.log(    ' p0:' + $scope.vectorText(p0)
-                  + ' p1:' + $scope.vectorText(p1)
-                  + ' p2:' + $scope.vectorText(p2)
-                  + ' p3:' + $scope.vectorText(p3));
-    var pointList = [];
-    pointList.push(p0);
-    pointList.push(p1);
-    pointList.push(p2);
-    pointList.push(p3);
-    pointList.map(function(p) {
-      geometry.vertices.push(p);
-    });
-
-    geometry.faces.push( new THREE.Face3( 0, 1, 2 ) );
-    geometry.faces.push( new THREE.Face3( 3, 0, 2 ) );
+    var gc = 4; // factor = 1 (2^1+1=3)
+    var points = new Array();
+    var deltaX = $scope.gridWidthPixels / (gc-1);
+    var deltaZ = $scope.gridHeightPixels / (gc-1);
+    for(var x = 0; x < gc; x++) {
+      points[x] = new Array();
+      for(var z = 0; z < gc; z++) {
+        var xp = (-1 * ($scope.gridWidthPixels/2)) + (deltaX * x);
+        var zp = (-1 * ($scope.gridHeightPixels/2)) + (deltaZ * z);
+        var p0 = new THREE.Vector3( xp, 0, zp);
+        points[x][z] = p0;
+        //geometry.vertices.push(p0);
+        console.log(x + ':' + z + ' p=' + $scope.vectorText(p0));
+      }
+    }
+    // render faces
+    var i=0;
+    for(var x = 0; x < (gc-1); x++) {
+      for (var z = 0; z < (gc-1); z++) {
+        // get the square
+        var p0 = points[x][z];
+        var p1 = points[x+1][z];
+        var p2 = points[x+1][z+1];
+        var p3 = points[x][z+1];
+        geometry.vertices.push(p0);
+        geometry.vertices.push(p1);
+        geometry.vertices.push(p2);
+        geometry.vertices.push(p3);
+        geometry.faces.push( new THREE.Face3( i + 0, i + 1, i + 2 ) );
+        geometry.faces.push( new THREE.Face3( i + 3, i + 0, i + 2 ) );
+        i += 4;
+      }
+    }
 
     geometry.computeBoundingSphere();
 
     //var geometry = new THREE.BoxGeometry( 100, 100, 100 );
 
     //var material = new THREE.MeshLambertMaterial( {color: 0xAAAACC} );
-    var material = new THREE.ShaderMaterial( {
-      uniforms: {},
-      vertexShader: document.getElementById( 'vertexShader' ).textContent,
-      fragmentShader: document.getElementById( 'fragmentShader' ).textContent,
-      derivatives: true
-    } );
     var material = new THREE.MeshBasicMaterial( { wireframe: true } );
     var planeObject = new THREE.Mesh( geometry, material );
     $scope.scene.add( planeObject );
@@ -85,7 +92,7 @@ app.controller("TerrainAppController", function($scope) {
     0.1,            // Near plane
     10000           // Far plane
   );
-  camera.position.set(0, (2 * $scope.gridWidthPixels) / 4, (-1 * $scope.gridWidthPixels) *2);
+  camera.position.set(0, (2 * $scope.gridWidthPixels) / 4, (-1 * $scope.gridWidthPixels) * 2);
   var sceneCenter = scene.position;
   console.log('scene center=' + $scope.vectorText(sceneCenter));
   camera.lookAt(sceneCenter);
